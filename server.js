0e333f67d2686820e74cbcba2180bdacaca2408f
ADEDOKUN ISAAC <Isaacadedokun111@gmail.com>
2026-05-24 03:24:09 +0100

    new update on server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
+import fs from "fs";
 
 const app = express();
 const PORT = 3000;
@@ -13,9 +14,56 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/api/tasks", (req, res) => {
-  res.json({ status: "success", message: "taskflow API is running" });
+function readTasks() {
+  const data = fs.readFileSync(tasksFile, 'utf-8');
+  return JSON.parse(data);
+}
+function writeTasks(tasks) {
+  fs.writeFileSync(tasksFile, JSON.stringify(tasks, null, 2));
+}
+
+app.get('/api/tasks', (req, res) => {
+  const tasks = readTasks();
+  res.json(tasks);
 });
+
+app.post('/api/tasks', (req, res) => {
+  const tasks = readTasks();
+  const newTask = {
+    id: Date.now(),
+    title: req.body.title,
+    completed: false
+  };
+   tasks.push(newTask);
+  writeTasks(tasks);
+
+  res.json(newTask);
+});
+
+app.put('/api/tasks/:id', (req, res) => {
+  const tasks = readTasks();
+  const id = Number(req.params.id);
+
+  const updatedTasks = tasks.map(task =>
+    task.id === id ? { ...task, ...req.body } : task
+  );
+
+   writeTasks(updatedTasks);
+
+  res.json({ message: 'Task updated' });
+});
+
+app.delete('/api/tasks/:id', (req, res) => {
+  const tasks = readTasks();
+  const id = Number(req.params.id);
+
+  const filtered = tasks.filter(task => task.id !== id);
+  writeTasks(filtered);
+
+  res.json({ message: 'Task deleted' });
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
